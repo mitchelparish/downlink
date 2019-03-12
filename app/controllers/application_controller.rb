c41138791d9930fa12e76c0d459fc862de42e9b0
mitchelparish <mitchelparish@gmail.com>
2019-03-12 13:37:14 -0600

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret" #change later, see what impacts changes might have?
   end
 
   get '/' do
     erb :index
   end
 
+  helpers do
+    # def redirect_if_not_logged_in
+    #   if !logged_in?
+    #     redirect "/login?error=You have to be logged in to do that"
+    #   end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
