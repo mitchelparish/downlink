b733ae738f8d2f0ebd8a8d0b6615a69388d5e78f
mitchelparish <mitchelparish@gmail.com>
2019-03-12 14:33:12 -0600

    migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,48 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect to 'users/show'
+    end
+  end
 
-end 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :password => params[:password])
+      @user.save
+        session[:user_id] = @user.id
+        redirect to '/users/show'
+      end
+    end
+
+    get '/login' do
+      if !logged_in?
+        erb :'users/login'
+      else
+        redirect '/users/show'
+      end
+    end
+
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/users/show"
+      else
+        redirect to '/signup'
+      end
+    end
+
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
+    end
+  end
