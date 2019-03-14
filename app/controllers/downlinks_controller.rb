67d1e9415338536c7c09caf219ced5d53d51c84d
mitchelparish <mitchelparish@gmail.com>
2019-03-14 12:32:23 -0600

    edit view

--- app/controllers/downlinks_controller.rb
+++ app/controllers/downlinks_controller.rb
@@ -41,18 +41,18 @@ class DownlinksController < ApplicationController
     end
   end
 
-  # get '/downlinks/:id/edit' do
-  #   if logged_in?
-  #     @downlink = Downlink.find_by_id(params[:id])
-  #     if @downlink && @downlink.user == current_user
-  #       erb :'downlinks/edit'
-  #     else
-  #       redirect to 'users/show'
-  #     end
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
+  get '/downlinks/:id/edit' do
+    if logged_in?
+      @downlink = Downlink.find_by_id(params[:id])
+      if @downlink && @downlink.user == current_user
+        erb :'downlinks/edit'
+      else
+        redirect to '/users/show'
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
   # patch '/downlinks/:id' do
   #   if logged_in?
@@ -75,15 +75,15 @@ class DownlinksController < ApplicationController
   #   end
   # end
 
-  # delete '/downlinks/:id/delete' do
-  #   if logged_in?
-  #     @downlink = Downlink.find_by_id(params[:id])
-  #     if @downlink && @downlink.user == current_user
-  #       @downlink.delete
-  #     end
-  #     redirect to "/users/#{current_user.slug}"
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
+  delete '/downlinks/:id/delete' do
+    if logged_in?
+      @downlink = Downlink.find_by_id(params[:id])
+      if current_user.id == @downlink.user_id
+        @downlink.delete
+      end
+      redirect to '/users/show'
+    else
+      redirect to '/login'
+    end
+  end
 end
