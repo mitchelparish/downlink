a05506a11dbe182719717eaed7512665507b6f95
mitchelparish <mitchelparish@gmail.com>
2019-03-14 19:50:46 -0600

    initial css

--- notes.rb
+++ notes.rb
@@ -1,91 +1,15 @@
-class DownlinksController < ApplicationController
+<!doctype html>
+<html>
+  <head>
+    <title>Establishing downlink...</title>
+  </head>
+  <body>
+    <h2>Login to establish your downlink:</h2>
+    <form action="/login" method="POST">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit">
+      </form>
 
-  get '/downlinks/show' do
-    if logged_in?
-        @downlinks = Downlink.all
-        erb :'downlinks/show'
-      else
-        redirect to '/login'
-      end
-    end
 
-    get '/downlinks/new' do
-      if logged_in?
-        erb :'downlinks/new'
-      else
-        redirect to '/login'
-      end
-    end
-
-    post '/downlinks/new' do
-      if logged_in?
-        if params[:content] == ""
-          redirect to "/downlinks/new"
-        else
-          @downlink = current_user.downlinks.build(content: params[:content])
-          if @downlink.save
-            redirect to "/downlinks/#{@downlink.id}"
-          else
-            redirect to "/downlinks/new"
-          end
-        end
-      else
-        redirect to '/login'
-      end
-    end
-
-    get '/downlinks/:id' do
-      if logged_in?
-        @downlink = Downlink.find_by_id(params[:id])
-        erb :'downlinks/show'
-      else
-        redirect to '/login'
-      end
-    end
-
-    get '/downlinks/:id/edit' do
-      if logged_in?
-        @downlink = Downlink.find_by_id(params[:id])
-        if @downlink && @downlink.user == current_user
-          erb :'downlinks/edit'
-        else
-          redirect to 'users/show'
-        end
-      else
-        redirect to '/login'
-      end
-    end
-
-    patch '/downlinks/:id' do
-      if logged_in?
-        if params[:content] == "" # or || any other param empty
-          redirect to "/downlinks/#{params[:id]}/edit"
-        else
-          @downlink = Downlink.find_by_id(params[:id])
-          if @downlink && @downlink.user == current_user
-            if @downlink.update(content: params[:content])
-              redirect to "/downlinks/#{@downlink.id}"
-            else
-              redirect to "/downlinks/#{@downlink.id}/edit"
-            end
-          else
-            redirect to '/users/show'
-          end
-        end
-      else
-        redirect to '/login'
-      end
-    end
-
-    delete '/downlinks/:id/delete' do
-      if logged_in?
-        @downlink = Downlink.find_by_id(params[:id])
-        if @downlink && @downlink.user == current_user
-          @downlink.delete
-        end
-        redirect to '/users/show'
-      else
-        redirect to '/login'
-      end
-    end
-  end
+</html>
